1a60974cacb2e628a2e324a9bcc5ce69c3d84736
Rajat Saxena <rajat@raynstudios.in>
2018-10-20 20:13:14 +0530

    Removed Bootstrap from Dashboard

--- dashboard/src/index.js
+++ dashboard/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "bootstrap/dist/css/bootstrap.css";
-import "bootstrap/dist/css/bootstrap-theme.css";
+// import "bootstrap/dist/css/bootstrap.css";
+// import "bootstrap/dist/css/bootstrap-theme.css";
 import "./index.css";
 import App from "./App";
 // import { unregister } from "./registerServiceWorker";
